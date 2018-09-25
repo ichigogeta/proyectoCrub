30b85d5c2e69f0281e01da1421f08ae493f97f96
Javier Garcia <soporte5@xerintel.es>
2018-09-25 18:45:54 +0200

    plantilla vacia de referencia

---
+++ resources/views/vacia.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('meta')
+    <meta name="description"
+          content="Projecto de intranet xerintel con laravel">
+    <meta name="keywords" content="xerintel,laravel,projecto,intranet,web">
+    <meta name="author" content="Javier García">
+@endsection
+
+@section('title')
+    Ever+ | The finest HTML5 Template
+@endsection
+
+@section('content')
+
+@endsection
