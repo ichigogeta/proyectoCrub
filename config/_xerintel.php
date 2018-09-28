d97be1900f3fd6e263850c71f772827de3b408f9
Habier <14944667+Habier@users.noreply.github.com>
2018-09-28 11:58:02 +0200

    Añadidos todos los campos email necesarios

--- config/_xerintel.php
+++ config/_xerintel.php
@@ -4,16 +4,33 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Name
+    | Email del cliente
     |--------------------------------------------------------------------------
     |
-    | This value is the name of your application. This value is used when the
-    | framework needs to place the application's name in a notification or
-    | any other location as required by the application or its packages.
+    | Avisos, formularío de contacto y otras necesidades.
     |
     */
 
     'mail_cliente' => '',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Email de la aplicación
+    |--------------------------------------------------------------------------
+    |
+    | Remitente de los emails enviados via PHP
+    |
+    */
+    'app_mail' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Email de la aplicación
+    |--------------------------------------------------------------------------
+    |
+    | Remitente de los emails enviados via PHP
+    |
+    */
+    'app_mail_name' => 'App Xerintel',
 
 ];
