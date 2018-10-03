be481e5a56298e12850210650d8042a89a717927
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-10-03 11:48:21 +0200

    barra cookies abajo.

--- resources/views/layouts/cookie_law.blade.php
+++ resources/views/layouts/cookie_law.blade.php
@@ -1,7 +1,8 @@
-<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+<link rel="stylesheet" type="text/css"
+      href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"/>
 <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
 <script>
-    window.addEventListener("load", function(){
+    window.addEventListener("load", function () {
         window.cookieconsent.initialise({
             "palette": {
                 "popup": {
@@ -11,12 +12,13 @@
                     "background": "#14a7d0"
                 }
             },
-            "position": "top",
+            "position": "bottom",
             "content": {
                 "message": "Este sitio web utiliza cookies para ofrecer el 100% de su funcionalidad.",
                 "dismiss": "Entendido",
                 "link": "Más información",
                 "href": "http://politicadecookies.com/cookies.php"
             }
-        })});
+        })
+    });
 </script>
