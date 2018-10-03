df7b9d5d59089e35ebf7f648b427b3e38dd2f1a0
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-10-03 13:57:50 +0200

    error personalizado 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title')
+    Esto no existe
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6 text-center col-md-offset-3">
+            <div class="title-block">
+                <h1>¡Vaya!</h1>
+            </div>
+            <p>¡Esta página ha sido movida o no existe!</p>
+            <h5>También conocido como error: 404</h5>
+        </div>
+    </div>
+@endsection
