47d0c8546355578cfaf697653097b00b9151ff62
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-10-04 14:37:01 +0200

    Cambio de sitio del logo voyager, para ser futuro xerintel.

--- resources/views/vendor/voyager/login.blade.php
+++ resources/views/vendor/voyager/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="robots" content="none" />
+    <meta name="robots" content="none"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta name="description" content="admin login">
     <title>Admin - {{ Voyager::setting("admin.title") }}</title>
@@ -14,23 +14,40 @@
     @endif
     <style>
         body {
-            background-image:url('{{ Voyager::image( Voyager::setting("admin.bg_image"), voyager_asset("images/bg.jpg") ) }}');
+            background-image: url('{{ Voyager::image( Voyager::setting("admin.bg_image"), voyager_asset("images/bg.jpg") ) }}');
             background-color: {{ Voyager::setting("admin.bg_color", "#FFFFFF" ) }};
         }
+
         body.login .login-sidebar {
-            border-top:5px solid {{ config('voyager.primary_color','#22A7F0') }};
+            border-top: 5px solid{{ config('voyager.primary_color','#22A7F0') }};
         }
+
         @media (max-width: 767px) {
             body.login .login-sidebar {
-                border-top:0px !important;
-                border-left:5px solid {{ config('voyager.primary_color','#22A7F0') }};
+                border-top: 0px !important;
+                border-left: 5px solid{{ config('voyager.primary_color','#22A7F0') }};
             }
         }
-        body.login .form-group-default.focused{
-            border-color:{{ config('voyager.primary_color','#22A7F0') }};
+
+        body.login .form-group-default.focused {
+            border-color: {{ config('voyager.primary_color','#22A7F0') }};
         }
-        .login-button, .bar:before, .bar:after{
-            background:{{ config('voyager.primary_color','#22A7F0') }};
+
+        .login-button, .bar:before, .bar:after {
+            background: {{ config('voyager.primary_color','#22A7F0') }};
+        }
+
+        body.login .logo-title-container {
+            margin-top: 0;
+            bottom: 47%;
+            left: 25%;
+        }
+
+        @media (max-width: 790px) {
+            body.login .logo-title-container {
+                bottom: 48%;
+                left: 5%;
+            }
         }
     </style>
 
@@ -46,9 +63,11 @@
                     <div class="logo-title-container">
                         <?php $admin_logo_img = Voyager::setting('admin.icon_image', ''); ?>
                         @if($admin_logo_img == '')
-                        <img class="img-responsive pull-left flip logo hidden-xs animated fadeIn" src="{{ voyager_asset('images/logo-icon-light.png') }}" alt="Logo Icon">
+                            <img class="img-responsive pull-left flip logo hidden-xs animated fadeIn"
+                                 src="{{ voyager_asset('images/logo-icon-light.png') }}" alt="Logo Icon">
                         @else
-                        <img class="img-responsive pull-left flip logo hidden-xs animated fadeIn" src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon">
+                            <img class="img-responsive pull-left flip logo hidden-xs animated fadeIn"
+                                 src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon">
                         @endif
                         <div class="copy animated fadeIn">
                             <h1>{{ Voyager::setting('admin.title', 'Voyager') }}</h1>
@@ -70,35 +89,38 @@
                     <div class="form-group form-group-default" id="emailGroup">
                         <label>{{ __('voyager::generic.email') }}</label>
                         <div class="controls">
-                            <input type="text" name="email" id="email" value="{{ old('email') }}" placeholder="{{ __('voyager::generic.email') }}" class="form-control" required>
-                         </div>
+                            <input type="text" name="email" id="email" value="{{ old('email') }}"
+                                   placeholder="{{ __('voyager::generic.email') }}" class="form-control" required>
+                        </div>
                     </div>
 
                     <div class="form-group form-group-default" id="passwordGroup">
                         <label>{{ __('voyager::generic.password') }}</label>
                         <div class="controls">
-                            <input type="password" name="password" placeholder="{{ __('voyager::generic.password') }}" class="form-control" required>
+                            <input type="password" name="password" placeholder="{{ __('voyager::generic.password') }}"
+                                   class="form-control" required>
                         </div>
                     </div>
 
                     <button type="submit" class="btn btn-block login-button">
-                        <span class="signingin hidden"><span class="voyager-refresh"></span> {{ __('voyager::login.loggingin') }}...</span>
+                        <span class="signingin hidden"><span
+                                    class="voyager-refresh"></span> {{ __('voyager::login.loggingin') }}...</span>
                         <span class="signin">{{ __('voyager::generic.login') }}</span>
                     </button>
 
-              </form>
+                </form>
 
-              <div style="clear:both"></div>
+                <div style="clear:both"></div>
 
-              @if(!$errors->isEmpty())
-              <div class="alert alert-red">
-                <ul class="list-unstyled">
-                    @foreach($errors->all() as $err)
-                    <li>{{ $err }}</li>
-                    @endforeach
-                </ul>
-              </div>
-              @endif
+                @if(!$errors->isEmpty())
+                    <div class="alert alert-red">
+                        <ul class="list-unstyled">
+                            @foreach($errors->all() as $err)
+                                <li>{{ $err }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
 
             </div> <!-- .login-container -->
 
@@ -110,7 +132,7 @@
     var form = document.forms[0];
     var email = document.querySelector('[name="email"]');
     var password = document.querySelector('[name="password"]');
-    btn.addEventListener('click', function(ev){
+    btn.addEventListener('click', function (ev) {
         if (form.checkValidity()) {
             btn.querySelector('.signingin').className = 'signingin';
             btn.querySelector('.signin').className = 'signin hidden';
@@ -122,18 +144,18 @@
     document.getElementById('emailGroup').classList.add("focused");
 
     // Focus events for email and password fields
-    email.addEventListener('focusin', function(e){
+    email.addEventListener('focusin', function (e) {
         document.getElementById('emailGroup').classList.add("focused");
     });
-    email.addEventListener('focusout', function(e){
-       document.getElementById('emailGroup').classList.remove("focused");
+    email.addEventListener('focusout', function (e) {
+        document.getElementById('emailGroup').classList.remove("focused");
     });
 
-    password.addEventListener('focusin', function(e){
+    password.addEventListener('focusin', function (e) {
         document.getElementById('passwordGroup').classList.add("focused");
     });
-    password.addEventListener('focusout', function(e){
-       document.getElementById('passwordGroup').classList.remove("focused");
+    password.addEventListener('focusout', function (e) {
+        document.getElementById('passwordGroup').classList.remove("focused");
     });
 
 </script>
