95e19eb2c050f4be1201f29f5b424c4e4edb075b
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-10-04 13:50:34 +0200

    Cambios de traducción y extraccion del login.blade

--- resources/lang/vendor/voyager/es/theme.php
+++ resources/lang/vendor/voyager/es/theme.php
@@ -2,5 +2,5 @@
 
 return [
     'footer_copyright'  => 'Hecho con <i class = "voyager-heart"> </i> por',
-    'footer_copyright2' => 'Hecho con ron e incluso más ron',
+    'footer_copyright2' => '', //Hecho con ron e incluso más ron
 ];
