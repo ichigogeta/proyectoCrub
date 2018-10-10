d473313c11a65609de22799995bf3eff5cb52abd
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-10-10 18:44:24 +0200

    Simplificación de layouts.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,28 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    @yield('meta')
-    <title>
-        @section('title')
-            {{ config('app.name', 'Laravel') }}
-        @show
-    </title>
-    @include('layouts.head')
-    <script src="{{ asset('js/app.js') }}"></script>
-</head>
+
+@include('layouts.head')
+
 <body id="app-layout">
+
 @yield('special_content')
 @include('layouts.navbar')
 @include('layouts.errors')
 @include('flash.normal')
+
 <div id="container" class="container">
     @yield('content')
-    {{--
-        @include('layouts.cookie_law')
-    --}}
+    {{-- @include('layouts.cookie_law')  --}}
 </div>
-<footer class="footer">
-    @include('layouts.footer')
-</footer>
+
+@include('layouts.footer')
 </body>
 </html>
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,24 +1,36 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<meta name="csrf-token" content="{{ csrf_token() }}">
-{{-- Favicon --}}
-<link rel="icon" href="{{asset('favicon.ico')}}" type="image/ico" sizes="16x16">
-<!-- Fonts -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel='stylesheet'
-      type='text/css'>
-<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+    @yield('meta')
+    <title>
+        @section('title')
+            {{ config('app.name', 'Laravel') }}
+        @show
+    </title>
 
-<!-- Styles -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-{{-- Optional theme --}}
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- Favicon --}}
+    <link rel="icon" href="{{asset('favicon.ico')}}" type="image/ico" sizes="16x16">
+    <!-- Fonts -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel='stylesheet'
+          type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 
-<!-- JavaScripts -->
-<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-{{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-<link href="{{url(mix('css/app.css')) }}" rel="stylesheet">
-{{--<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js"></script>--}}
-{{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    {{-- Optional theme --}}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+    <!-- JavaScripts -->
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{url(mix('css/app.css')) }}" rel="stylesheet">
+    {{--<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js"></script>--}}
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
