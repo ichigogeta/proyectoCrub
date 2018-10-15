569118c78b5838bf444c50a89ed40809aa33c15b
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-10-15 18:35:57 +0200

    Ejemplo de vista interna de la intranet modificable.

---
+++ resources/views/vendor/voyager/ejemplo/ejemplo.blade.php
@@ -0,0 +1,11 @@
+@extends('voyager::master')
+
+{{--Ver más https://laravelvoyager.com/academy/views/--}}
+{{--
+El nombre de la carpeta debe ser el slug del modelo, por ejemplo slides.
+El nombre del fichero varía según que queramos editar:
+browse.blade.php edit-add.blade.php
+--}}
+@section('content')
+    Aquí trabajaremos
+@endsection
