d9a12ee64e631caaa10bc3e8f79c4992d75072a1
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-10-17 09:55:49 +0200

    contenido de ejemplo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,5 +18,9 @@
         {{-- Esto es un comentario privado --}}
         <h1>Si puedes leer esto, es que se instaló bien.</h1>
         <p>Barra de cookies ocultada, ver más en views/layouts/app.blade.php</p>
+        <h2>Enlaces pre-generados</h2>
+        <a href="{{url('/pagina/sobre-nosotros')}}">Sobre nosotros</a> <br>
+        <a href="{{url('/pagina/politica-de-privacidad')}}">Política de Privacidad</a> <br>
+        <a href="{{url('/pagina/aviso-legal-y-cookies')}}">Aviso Legal</a>
     </div>
 @endsection
