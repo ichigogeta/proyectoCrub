16eda41d99ef36bb370055accfb976c7d78a4b68
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-10-17 10:30:21 +0200

    Optimizaciones para la próxima instalación

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,5 +22,13 @@
         <a href="{{url('/pagina/sobre-nosotros')}}">Sobre nosotros</a> <br>
         <a href="{{url('/pagina/politica-de-privacidad')}}">Política de Privacidad</a> <br>
         <a href="{{url('/pagina/aviso-legal-y-cookies')}}">Aviso Legal</a>
+
+        <h2>¿Quieres ir a la intranet?</h2>
+        <a href="{{url('/intranet')}}"> {{url('/intranet')}}</a>
+        <h3>¿Fallan los enlaces o las fotos?</h3>
+        <p>
+            Revisa la URL que pusiste en el fichero <strong>.env</strong>
+        </p>
+
     </div>
 @endsection
