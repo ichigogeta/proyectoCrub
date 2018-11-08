ee7a11f68adbae0045b3cb5168f0e2765dd35954
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-11-08 18:12:41 +0100

    añadido modelo Slide por ser de uso comun

---
+++ app/Slide.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Slide extends Model
+{
+    public $timestamps = false; //false significa no usar fechas de creacion y modificacion en la tabla. default=true.
+
+}
