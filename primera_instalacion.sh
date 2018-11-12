bd5237f138b1f6015edf136991994bfa72dd96db
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-11-12 11:40:13 +0100

    Añadido script para primera instalación en produción

---
+++ primera_instalacion.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Iniciado configurador. Esto puede tardar unos minutos."
+sleep 0.5
+echo "Recuerda haber creado la base de datos, y copiar las claves."
+sleep 0.5
+echo "¿Es correcta la url de la aplicación?"
+sleep 0.5
+echo "¿Está el debug en false?"
+
+git config --global user.email "proyectos@xerintel.es"
+git config --global user.name "Xerintel Proyectos"
+composer install --no-dev
+php artisan xerintel:install
