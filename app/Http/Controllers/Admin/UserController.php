89c9ab732f48cd7d3525438ca87fcde8a9acc75c
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-11-20 14:39:40 +0100

    Solo un admin puede poner el rol admin

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
+
+class UserController extends VoyagerBaseController
+{
+    public function update(Request $request, $id)
+    {
+        $this->youShallNotPass($request);
+        return parent::update($request, $id);
+    }
+
+    public function store(Request $request)
+    {
+        $this->youShallNotPass($request);
+        return parent::store($request);
+    }
+
+    private function youShallNotPass($request)
+    {
+        dd(Auth::user());
+        if (Auth::user()->role_id != 1) //no es admin
+        {
+            if ($request->get('role_id') == 1)
+                $this->detectadaAccionProhibida();
+
+            $array = $request->get('user_belongstomany_role_relationship');
+            if (!empty($array))
+                foreach ($array as $role) {
+                    if ($role == 1)
+                        $this->detectadaAccionProhibida();
+                }
+
+        }
+    }
+
+    private function detectadaAccionProhibida()
+    {
+        die('Detectada acción prohibida');
+    }
+}
