bdef1d7ba4205ce318749e08145ac0f5ae7d9c5c
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-11-21 11:43:45 +0100

    Añadido Font Awesome 5.5

--- config/voyager.php
+++ config/voyager.php
@@ -12,14 +12,14 @@ return [
 
     'user' => [
         'add_default_role_on_register' => true,
-        'default_role'                 => 'user',
+        'default_role' => 'user',
         // Set `namespace` to `null` to use `config('auth.providers.users.model')` value
         // Set `namespace` to a class to override auth user model.
         // However make sure the appointed class must ready to use before installing voyager.
         // Otherwise `php artisan voyager:install` will fail with class not found error.
-        'namespace'                    => null,
-        'default_avatar'               => 'users/default.png',
-        'redirect'                     => '/intranet',
+        'namespace' => null,
+        'default_avatar' => 'users/default.png',
+        'redirect' => '/intranet',
     ],
 
     /*
@@ -148,17 +148,17 @@ return [
         // Add custom list items to navbar's dropdown
         'navbar_items' => [
             'Perfil' => [
-                'route'      => 'voyager.profile',
-                'classes'    => 'class-full-of-rum',
+                'route' => 'voyager.profile',
+                'classes' => 'class-full-of-rum',
                 'icon_class' => 'voyager-person',
             ],
             'Volver a la Web' => [
-                'route'        => '/',
-                'icon_class'   => 'voyager-home',
+                'route' => '/',
+                'icon_class' => 'voyager-home',
                 'target_blank' => true,
             ],
             'Cerrar Sesión' => [
-                'route'      => 'voyager.logout',
+                'route' => 'voyager.logout',
                 'icon_class' => 'voyager-power',
             ],
         ],
@@ -209,7 +209,7 @@ return [
 
     // Here you can specify additional assets you would like to be included in the master.blade
     'additional_css' => [
-        //'css/custom.css',
+        'css/fontawesome-5.5/css/all.css',
     ],
 
     'additional_js' => [
@@ -217,12 +217,12 @@ return [
     ],
 
     'googlemaps' => [
-         'key'    => env('GOOGLE_MAPS_KEY', ''),
-         'center' => [
-             'lat' => env('GOOGLE_MAPS_DEFAULT_CENTER_LAT', '32.715738'),
-             'lng' => env('GOOGLE_MAPS_DEFAULT_CENTER_LNG', '-117.161084'),
-         ],
-         'zoom' => env('GOOGLE_MAPS_DEFAULT_ZOOM', 11),
-     ],
+        'key' => env('GOOGLE_MAPS_KEY', ''),
+        'center' => [
+            'lat' => env('GOOGLE_MAPS_DEFAULT_CENTER_LAT', '32.715738'),
+            'lng' => env('GOOGLE_MAPS_DEFAULT_CENTER_LNG', '-117.161084'),
+        ],
+        'zoom' => env('GOOGLE_MAPS_DEFAULT_ZOOM', 11),
+    ],
 
 ];
