fe819a1a35f9f03777ceb0794fba1b418c8a0a9b
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-11-26 11:51:04 +0100

    plantilla de modulo vacio

---
+++ resources/views/intranet/vacia.blade.php
@@ -0,0 +1,17 @@
+@extends('voyager::master')
+
+
+@section('page_title', 'Modulo Vacío')
+
+@section('page_header')
+    <h1 class="page-title">
+        {{-- usa el icono que quieras --}}
+        <i class="voyager-documentation"></i>
+        Título Modulo Vacío
+    </h1>
+@stop
+
+
+@section('content')
+    <h1>Contenido Vacío</h1>
+@endsection
