dc61b64179aa1726c189d5a80dbf2e110b30a10c
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-11-29 12:54:52 +0100

    plantillas de logins sociales faltantes

---
+++ resources/views/auth/socialite.blade.php
@@ -0,0 +1,15 @@
+<div class="form-group row">
+    <label for="name" class="col-md-4 control-label">Entrar con</label>
+                               
+    <div class="col-md-6">
+
+        <a href="{{ url('login/facebook') }}" class="btn btn-social-icon btn-facebook">
+            <i class="fa fa-facebook"></i>
+        </a>
+
+        <a href="{{ url('login/google') }}" class="btn btn-social-icon btn-google-plus">
+            <i class="fa fa-google-plus"></i>
+        </a>
+                                   
+    </div>
+</div>
