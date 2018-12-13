db43a3c477f054e01240afe2e4e3b491b97763f0
Javier Garcia <javiergarcia.jgc@gmail.com>
2018-12-13 11:02:28 +0100

    arreglo json_encode en details

--- database/seeds/PagesTableSeeder.php
+++ database/seeds/PagesTableSeeder.php
@@ -21,14 +21,14 @@ class PagesTableSeeder extends Seeder
         $dataType = $this->dataType('slug', 'pages');
         if (!$dataType->exists) {
             $dataType->fill([
-                'name' => 'pages',
+                'name'                  => 'pages',
                 'display_name_singular' => __('voyager::seeders.data_types.page.singular'),
-                'display_name_plural' => __('voyager::seeders.data_types.page.plural'),
-                'icon' => 'voyager-file-text',
-                'model_name' => 'TCG\\Voyager\\Models\\Page',
-                'controller' => '',
-                'generate_permissions' => 1,
-                'description' => '',
+                'display_name_plural'   => __('voyager::seeders.data_types.page.plural'),
+                'icon'                  => 'voyager-file-text',
+                'model_name'            => 'TCG\\Voyager\\Models\\Page',
+                'controller'            => '',
+                'generate_permissions'  => 1,
+                'description'           => '',
             ])->save();
         }
 
@@ -37,102 +37,97 @@ class PagesTableSeeder extends Seeder
         $dataRow = $this->dataRow($pageDataType, 'id');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type' => 'number',
+                'type'         => 'number',
                 'display_name' => __('voyager::seeders.data_rows.id'),
-                'required' => 1,
-                'browse' => 0,
-                'read' => 0,
-                'edit' => 0,
-                'add' => 0,
-                'delete' => 0,
-                'details' => '',
-                'order' => 1,
+                'required'     => 1,
+                'browse'       => 0,
+                'read'         => 0,
+                'edit'         => 0,
+                'add'          => 0,
+                'delete'       => 0,
+                'order'        => 1,
             ])->save();
         }
 
         $dataRow = $this->dataRow($pageDataType, 'author_id');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type' => 'text',
+                'type'         => 'text',
                 'display_name' => __('voyager::seeders.data_rows.author'),
-                'required' => 1,
-                'browse' => 0,
-                'read' => 0,
-                'edit' => 0,
-                'add' => 0,
-                'delete' => 0,
-                'details' => '',
-                'order' => 2,
+                'required'     => 1,
+                'browse'       => 0,
+                'read'         => 0,
+                'edit'         => 0,
+                'add'          => 0,
+                'delete'       => 0,
+                'order'        => 2,
             ])->save();
         }
 
         $dataRow = $this->dataRow($pageDataType, 'title');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type' => 'text',
+                'type'         => 'text',
                 'display_name' => __('voyager::seeders.data_rows.title'),
-                'required' => 1,
-                'browse' => 1,
-                'read' => 1,
-                'edit' => 1,
-                'add' => 1,
-                'delete' => 1,
-                'details' => '',
-                'order' => 3,
+                'required'     => 1,
+                'browse'       => 1,
+                'read'         => 1,
+                'edit'         => 1,
+                'add'          => 1,
+                'delete'       => 1,
+                'order'        => 3,
             ])->save();
         }
 
         $dataRow = $this->dataRow($pageDataType, 'excerpt');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type' => 'text_area',
+                'type'         => 'text_area',
                 'display_name' => __('voyager::seeders.data_rows.excerpt'),
-                'required' => 1,
-                'browse' => 0,
-                'read' => 1,
-                'edit' => 1,
-                'add' => 1,
-                'delete' => 1,
-                'details' => '',
-                'order' => 4,
+                'required'     => 1,
+                'browse'       => 0,
+                'read'         => 1,
+                'edit'         => 1,
+                'add'          => 1,
+                'delete'       => 1,
+                'order'        => 4,
             ])->save();
         }
 
         $dataRow = $this->dataRow($pageDataType, 'body');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type' => 'rich_text_box',
+                'type'         => 'rich_text_box',
                 'display_name' => __('voyager::seeders.data_rows.body'),
-                'required' => 1,
-                'browse' => 0,
-                'read' => 1,
-                'edit' => 1,
-                'add' => 1,
-                'delete' => 1,
-                'details' => '',
-                'order' => 5,
+                'required'     => 1,
+                'browse'       => 0,
+                'read'         => 1,
+                'edit'         => 1,
+                'add'          => 1,
+                'delete'       => 1,
+                'order'        => 5,
             ])->save();
         }
 
         $dataRow = $this->dataRow($pageDataType, 'slug');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type' => 'text',
+                'type'         => 'text',
                 'display_name' => __('voyager::seeders.data_rows.slug'),
-                'required' => 1,
-                'browse' => 0,
-                'read' => 1,
-                'edit' => 1,
-                'add' => 1,
-                'delete' => 1,
-                'details' => json_encode([
+                'required'     => 1,
+                'browse'       => 0,
+                'read'         => 1,
+                'edit'         => 1,
+                'add'          => 1,
+                'delete'       => 1,
+                'details'      => [
                     'slugify' => [
                         'origin' => 'title',
                     ],
                     'validation' => [
-                        'rule' => 'unique:pages,slug',
+                        'rule'  => 'unique:pages,slug',
                     ],
-                ]),
+                ],
                 'order' => 6,
             ])->save();
         }
@@ -140,53 +135,51 @@ class PagesTableSeeder extends Seeder
         $dataRow = $this->dataRow($pageDataType, 'meta_description');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type' => 'text',
+                'type'         => 'text',
                 'display_name' => __('voyager::seeders.data_rows.meta_description'),
-                'required' => 1,
-                'browse' => 0,
-                'read' => 1,
-                'edit' => 1,
-                'add' => 1,
-                'delete' => 1,
-                'details' => '',
-                'order' => 7,
+                'required'     => 1,
+                'browse'       => 0,
+                'read'         => 1,
+                'edit'         => 1,
+                'add'          => 1,
+                'delete'       => 1,
+                'order'        => 7,
             ])->save();
         }
 
         $dataRow = $this->dataRow($pageDataType, 'meta_keywords');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type' => 'text',
+                'type'         => 'text',
                 'display_name' => __('voyager::seeders.data_rows.meta_keywords'),
-                'required' => 1,
-                'browse' => 0,
-                'read' => 1,
-                'edit' => 1,
-                'add' => 1,
-                'delete' => 1,
-                'details' => '',
-                'order' => 8,
+                'required'     => 1,
+                'browse'       => 0,
+                'read'         => 1,
+                'edit'         => 1,
+                'add'          => 1,
+                'delete'       => 1,
+                'order'        => 8,
             ])->save();
         }
 
         $dataRow = $this->dataRow($pageDataType, 'status');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type' => 'select_dropdown',
+                'type'         => 'select_dropdown',
                 'display_name' => __('voyager::seeders.data_rows.status'),
-                'required' => 1,
-                'browse' => 1,
-                'read' => 1,
-                'edit' => 1,
-                'add' => 1,
-                'delete' => 1,
-                'details' => json_encode([
+                'required'     => 1,
+                'browse'       => 1,
+                'read'         => 1,
+                'edit'         => 1,
+                'add'          => 1,
+                'delete'       => 1,
+                'details'      => [
                     'default' => 'INACTIVE',
                     'options' => [
                         'INACTIVE' => 'INACTIVE',
-                        'ACTIVE' => 'ACTIVE',
+                        'ACTIVE'   => 'ACTIVE',
                     ],
-                ]),
+                ],
                 'order' => 9,
             ])->save();
         }
@@ -194,48 +187,45 @@ class PagesTableSeeder extends Seeder
         $dataRow = $this->dataRow($pageDataType, 'created_at');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type' => 'timestamp',
+                'type'         => 'timestamp',
                 'display_name' => __('voyager::seeders.data_rows.created_at'),
-                'required' => 1,
-                'browse' => 1,
-                'read' => 1,
-                'edit' => 0,
-                'add' => 0,
-                'delete' => 0,
-                'details' => '',
-                'order' => 10,
+                'required'     => 1,
+                'browse'       => 1,
+                'read'         => 1,
+                'edit'         => 0,
+                'add'          => 0,
+                'delete'       => 0,
+                'order'        => 10,
             ])->save();
         }
 
         $dataRow = $this->dataRow($pageDataType, 'updated_at');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type' => 'timestamp',
+                'type'         => 'timestamp',
                 'display_name' => __('voyager::seeders.data_rows.updated_at'),
-                'required' => 1,
-                'browse' => 0,
-                'read' => 0,
-                'edit' => 0,
-                'add' => 0,
-                'delete' => 0,
-                'details' => '',
-                'order' => 11,
+                'required'     => 1,
+                'browse'       => 0,
+                'read'         => 0,
+                'edit'         => 0,
+                'add'          => 0,
+                'delete'       => 0,
+                'order'        => 11,
             ])->save();
         }
 
         $dataRow = $this->dataRow($pageDataType, 'image');
         if (!$dataRow->exists) {
             $dataRow->fill([
-                'type' => 'image',
+                'type'         => 'image',
                 'display_name' => __('voyager::seeders.data_rows.page_image'),
-                'required' => 0,
-                'browse' => 1,
-                'read' => 1,
-                'edit' => 1,
-                'add' => 1,
-                'delete' => 1,
-                'details' => '',
-                'order' => 12,
+                'required'     => 0,
+                'browse'       => 1,
+                'read'         => 1,
+                'edit'         => 1,
+                'add'          => 1,
+                'delete'       => 1,
+                'order'        => 12,
             ])->save();
         }
 
@@ -243,17 +233,17 @@ class PagesTableSeeder extends Seeder
         $menu = Menu::where('name', 'admin')->firstOrFail();
         $menuItem = MenuItem::firstOrNew([
             'menu_id' => $menu->id,
-            'title' => __('voyager::seeders.menu_items.pages'),
-            'url' => '',
-            'route' => 'voyager.pages.index',
+            'title'   => __('voyager::seeders.menu_items.pages'),
+            'url'     => '',
+            'route'   => 'voyager.pages.index',
         ]);
         if (!$menuItem->exists) {
             $menuItem->fill([
-                'target' => '_self',
+                'target'     => '_self',
                 'icon_class' => 'voyager-file-text',
-                'color' => null,
-                'parent_id' => null,
-                'order' => 7,
+                'color'      => null,
+                'parent_id'  => null,
+                'order'      => 7,
             ])->save();
         }
 
@@ -261,51 +251,21 @@ class PagesTableSeeder extends Seeder
         Permission::generateFor('pages');
         //Content
         $page = Page::firstOrNew([
-            'slug' => 'sobre-nosotros',
+            'slug' => 'hello-world',
         ]);
         if (!$page->exists) {
             $page->fill([
                 'author_id' => 0,
-                'title' => 'Sobre Nosotros',
-                'excerpt' => 'Entradilla sobre nosotros',
-                'body' => '<p>Esto es una prueba del contenido que podrás añadir desde tu panel de administración.</p>',
-                'meta_description' => 'sobre nosotros',
-                'meta_keywords' => 'sobre, nosotros',
-                'status' => 'ACTIVE',
+                'title'     => 'Hello World',
+                'excerpt'   => 'Hang the jib grog grog blossom grapple dance the hempen jig gangway pressgang bilge rat to go on account lugger. Nelsons folly gabion line draught scallywag fire ship gaff fluke fathom case shot. Sea Legs bilge rat sloop matey gabion long clothes run a shot across the bow Gold Road cog league.',
+                'body'      => '<p>Hello World. Scallywag grog swab Cat o\'nine tails scuttle rigging hardtack cable nipper Yellow Jack. Handsomely spirits knave lad killick landlubber or just lubber deadlights chantey pinnace crack Jennys tea cup. Provost long clothes black spot Yellow Jack bilged on her anchor league lateen sail case shot lee tackle.</p>
+<p>Ballast spirits fluke topmast me quarterdeck schooner landlubber or just lubber gabion belaying pin. Pinnace stern galleon starboard warp carouser to go on account dance the hempen jig jolly boat measured fer yer chains. Man-of-war fire in the hole nipperkin handsomely doubloon barkadeer Brethren of the Coast gibbet driver squiffy.</p>',
+                'image'            => 'pages/page1.jpg',
+                'meta_description' => 'Yar Meta Description',
+                'meta_keywords'    => 'Keyword1, Keyword2',
+                'status'           => 'ACTIVE',
             ])->save();
         }
-
-        $page = Page::firstOrNew([
-            'slug' => 'aviso-legal-y-cookies',
-        ]);
-
-        if (!$page->exists) {
-            $page->fill([
-                'author_id' => 0,
-                'title' => 'Aviso Legal y uso de cookies',
-                'excerpt' => '.',
-                'body' => '<p>.</p>',
-                'meta_description' => 'Aviso Legal y uso de cookies',
-                'meta_keywords' => 'aviso, legal, cookies',
-                'status' => 'ACTIVE',
-            ])->save();
-        }
-
-        $page = Page::firstOrNew([
-            'slug' => 'politica-de-privacidad',
-        ]);
-        if (!$page->exists) {
-            $page->fill([
-                'author_id' => 0,
-                'title' => 'Política de Privacidad',
-                'excerpt' => '.',
-                'body' => '<p>.</p>',
-                'meta_description' => 'Política de Privacidad',
-                'meta_keywords' => 'politica, privacidad',
-                'status' => 'ACTIVE',
-            ])->save();
-        }
-
     }
 
     /**
@@ -319,9 +279,9 @@ class PagesTableSeeder extends Seeder
     protected function dataRow($type, $field)
     {
         return DataRow::firstOrNew([
-            'data_type_id' => $type->id,
-            'field' => $field,
-        ]);
+                'data_type_id' => $type->id,
+                'field'        => $field,
+            ]);
     }
 
     /**
