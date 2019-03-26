78f0a35a343b7ec947e66536e00be4a0cd36ab5d
Javier Garcia <javiergarcia.jgc@gmail.com>
2019-03-26 10:03:58 +0100

    content builder 3.2 con "soporte multilenguaje"

--- resources/views/intranet/contentbuilderjs/scripts.blade.php
+++ resources/views/intranet/contentbuilderjs/scripts.blade.php
@@ -3,11 +3,20 @@
 <script src="{{asset('vendor/contentbuilder/contentbuilder/saveimages.js')}}" type="text/javascript"></script>
 
 <script type="text/javascript">
+    var container = '.content-builder-container'; //clase del contenedor
+    var urlGuardaImagenes = '{{route('intranet.contentbuilder.saveimage')}}';
+    var mainLang = 'es';
+    var currentLang = mainLang;
+    var bodyJsonInput = $('#body_i18n');
+    var bodyValues = JSON.parse(bodyJsonInput.val());
+    var multilenguaje = false;
+    var submitEnabled = true;
+    var nextLang;
+    var obj;
 
-    jQuery(document).ready(function ($) {
-
-        var obj = $.contentbuilder({
-            container: '.content-builder-container',
+    function createContentbuilder() {
+        return $.contentbuilder({
+            container: container,
             snippetData: '{{asset('vendor/contentbuilder/assets/minimalist-blocks/snippetlist.html')}}',
             snippetPathReplace: ['assets/minimalist-blocks/', '{{url('/vendor/contentbuilder/assets/minimalist-blocks/')}}/'],
             iconselect: '{{asset('vendor/contentbuilder/assets/ionicons/icons.html')}}',
@@ -15,6 +24,21 @@
             rowFormat: '<div class="row"></div>',
             framework: 'bootstrap'
         });
+    }
+
+    jQuery(document).ready(function ($) {
+
+        obj = createContentbuilder();
+
+        if ($('#body_i18n').length) {
+            //es multilenguaje
+            $('.form-edit-add').on('submit', function () {
+                bodyJsonInput.val(JSON.stringify(bodyValues));
+            });
+            multilenguaje = true;
+            submitEnabled = false;
+
+        }
 
         $('#btnViewSnippets').on('click', function () {
             obj.viewSnippets();
@@ -23,41 +47,65 @@
         $('#btnViewHtml').on('click', function () {
             obj.viewHtml();
         });
-        /*
-        $('#btnSave').on('click', function () {
-            save(obj);
-        });
-        */
+
         $('#enviar').on('click', function (e) {
             e.preventDefault(e);
 
-            if ($('body_i18n').length) {
-                //modo multilenguaje
+            if (multilenguaje) {
+                saveMultiLenguaje();
             }
             else {
-                save(obj, e);
+                saveNormal();
             }
         });
 
-        $('input[type=radio][name=i18n_selector]').change(function () {
-            alert(this.id);
 
+        $('input[type=radio][name=i18n_selector]').change(function () {
+            //$("html").fadeOut(1000);
+            nextLang = $("input[name='i18n_selector']:checked").attr('id');
+            console.log('Idioma clicado: ' + nextLang);
+            $(container).data('saveimages').save();
+        });
+
+        $(container).saveimages({
+            handler: urlGuardaImagenes, // handler for base64 image saving
+            onComplete: onComplete
         });
 
     });
 
-    function save(obj, e) {
-        $(".content-builder-container").saveimages({
-            handler: '{{route('intranet.contentbuilder.saveimage')}}', // handler for base64 image saving
-            onComplete: function () {
-                //alert('completado')
-                var html = obj.html(); //Get content
-                $('#body').val(html);
-                //alert('enviar');
-                $('.form-edit-add').submit();
-            }
-        });
-        $(".content-builder-container").data('saveimages').save();
+    function onComplete() {
+
+        if (!multilenguaje) { // modo normal
+            var html = obj.html(); //Get content
+            $('#body').val(html);
+            $('.form-edit-add').submit();
+        } else if (submitEnabled) { //multidioma al hacer submit
+            console.log('guardar');
+            bodyValues[currentLang] = obj.html();
+            $('.form-edit-add').submit(); // hay un evento on submit solo para multilenguaje
+        }
+        else { //multidioma al cambiar pestaña
+            bodyValues[currentLang] = obj.html();
+            currentLang = nextLang;
+            console.log('enviado');
+            obj.loadHtml(bodyValues[nextLang]);
+        }
+
+    }
+
+    function saveMultiLenguaje() {
+        // Guarda las imagenes del idioma actual, porque las otras se guardaron al cambiar de un idioma a otro.
+
+        //$("html").fadeOut(1000);
+        submitEnabled = true;
+        console.log('guardar?');
+
+        $(container).data('saveimages').save();
+    }
+
+    function saveNormal() {
+        $(container).data('saveimages').save();
         $("html").fadeOut(1000);//oculta la pagina mientras procesa
     }
 
