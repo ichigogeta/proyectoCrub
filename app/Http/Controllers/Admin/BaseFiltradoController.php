ed4446eb998085e29834947f566471ec9895a5e1
Javier Garcia <javiergarcia.jgc@gmail.com>
2019-04-02 12:10:27 +0200

    añadido el controlador con filtrado

---
+++ app/Http/Controllers/Admin/BaseFiltradoController.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use TCG\Voyager\Database\Schema\SchemaManager;
+use TCG\Voyager\Events\BreadDataDeleted;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
+
+class BaseFiltradoController extends VoyagerBaseController
+{
+    /*
+     * Reduce la necesidad de extender las funciones index y order
+     * Tendras que usar policies para administrar los privilegios de acceso, borrado etc.
+     * Ojo, las policies a veces, como en delete, no pasan el modelo completo sino el nombre, cuestión de rendimiento.
+     */
+
+    //protected $slug = 'nombre-slug'; // Cambiar si usas una ruta personalizada
+
+    /**
+     * @param Builder $query
+     * @return Builder
+     */
+    protected function miFiltro(Builder $query)
+    {
+        //Haz lo que quieras pero debes devolver $query siempre.
+
+        return $query;
+    }
+
+    public function index(Request $request)
+    {
+        // GET THE SLUG, ex. 'posts', 'pages', etc.
+        $slug = $this->getSlug($request);
+
+        // GET THE DataType based on the slug
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Check permission
+        $this->authorize('browse', app($dataType->model_name));
+
+        $getter = $dataType->server_side ? 'paginate' : 'get';
+
+        $search = (object)['value' => $request->get('s'), 'key' => $request->get('key'), 'filter' => $request->get('filter')];
+        $searchable = $dataType->server_side ? array_keys(SchemaManager::describeTable(app($dataType->model_name)->getTable())->toArray()) : '';
+        $orderBy = $request->get('order_by', $dataType->order_column);
+        $sortOrder = $request->get('sort_order', null);
+        $orderColumn = [];
+        if ($orderBy) {
+            $index = $dataType->browseRows->where('field', $orderBy)->keys()->first() + 1;
+            $orderColumn = [[$index, 'desc']];
+            if (!$sortOrder && isset($dataType->order_direction)) {
+                $sortOrder = $dataType->order_direction;
+                $orderColumn = [[$index, $dataType->order_direction]];
+            } else {
+                $orderColumn = [[$index, 'desc']];
+            }
+        }
+
+        // Next Get or Paginate the actual content from the MODEL that corresponds to the slug DataType
+        if (strlen($dataType->model_name) != 0) {
+            $model = app($dataType->model_name);
+            $query = $model::select('*');
+            $query = $this->miFiltro($query);
+
+            // If a column has a relationship associated with it, we do not want to show that field
+            $this->removeRelationshipField($dataType, 'browse');
+
+            if ($search->value && $search->key && $search->filter) {
+                $search_filter = ($search->filter == 'equals') ? '=' : 'LIKE';
+                $search_value = ($search->filter == 'equals') ? $search->value : '%' . $search->value . '%';
+                $query->where($search->key, $search_filter, $search_value);
+            }
+
+            if ($orderBy && in_array($orderBy, $dataType->fields())) {
+                $querySortOrder = (!empty($sortOrder)) ? $sortOrder : 'desc';
+                $dataTypeContent = call_user_func([
+                    $query->orderBy($orderBy, $querySortOrder),
+                    $getter,
+                ]);
+            } elseif ($model->timestamps) {
+                $dataTypeContent = call_user_func([$query->latest($model::CREATED_AT), $getter]);
+            } else {
+                $dataTypeContent = call_user_func([$query->orderBy($model->getKeyName(), 'DESC'), $getter]);
+            }
+
+            // Replace relationships' keys for labels and create READ links if a slug is provided.
+            $dataTypeContent = $this->resolveRelations($dataTypeContent, $dataType);
+        } else {
+            // If Model doesn't exist, get data from table name
+            $dataTypeContent = call_user_func([DB::table($dataType->name), $getter]);
+            $model = false;
+        }
+
+        // Check if BREAD is Translatable
+        if (($isModelTranslatable = is_bread_translatable($model))) {
+            $dataTypeContent->load('translations');
+        }
+
+        // Check if server side pagination is enabled
+        $isServerSide = isset($dataType->server_side) && $dataType->server_side;
+
+        // Check if a default search key is set
+        $defaultSearchKey = isset($dataType->default_search_key) ? $dataType->default_search_key : null;
+
+        $view = 'voyager::bread.browse';
+
+        if (view()->exists("voyager::$slug.browse")) {
+            $view = "voyager::$slug.browse";
+        }
+
+        return Voyager::view($view, compact(
+            'dataType',
+            'dataTypeContent',
+            'isModelTranslatable',
+            'search',
+            'orderBy',
+            'orderColumn',
+            'sortOrder',
+            'searchable',
+            'isServerSide',
+            'defaultSearchKey'
+        ));
+    }
+
+    /**
+     * Order BREAD items.
+     *
+     * @param string $table
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function order(Request $request)
+    {
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Check permission
+        $this->authorize('edit', app($dataType->model_name));
+
+        if (!isset($dataType->order_column) || !isset($dataType->order_display_column)) {
+            return redirect()
+                ->route("voyager.{$dataType->slug}.index")
+                ->with([
+                    'message' => __('voyager::bread.ordering_not_set'),
+                    'alert-type' => 'error',
+                ]);
+        }
+
+        $model = app($dataType->model_name);
+
+        /* Original
+        $results = $model->orderBy($dataType->order_column, $dataType->order_direction)->get();
+        */
+        $results = $this->miFiltro($model->orderBy($dataType->order_column, $dataType->order_direction))->get();
+
+        $display_column = $dataType->order_display_column;
+
+        $dataRow = Voyager::model('DataRow')->whereDataTypeId($dataType->id)->whereField($display_column)->first();
+
+        $view = 'voyager::bread.order';
+
+        if (view()->exists("voyager::$slug.order")) {
+            $view = "voyager::$slug.order";
+        }
+
+        return Voyager::view($view, compact(
+            'dataType',
+            'display_column',
+            'dataRow',
+            'results'
+        ));
+    }
+
+}
