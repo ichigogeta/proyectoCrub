c8e30fb5b8ed8ddcaea510523a719583deb5ad9f
Javier Garcia <javiergarcia.jgc@gmail.com>
2019-06-06 14:57:51 +0200

    comprobar active 1 como a Juan le gusta

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,7 +11,7 @@ class IndexController extends Controller
     public function index()
     {
         //$posts = Post::whereStatus('PUBLISHED')->orderBy('created_at', 'desc')->limit(3)->get();
-        //$slides = Slide::where('active')->orderBy('order')->get();
+        //$slides = Slide::where('active', 1)->orderBy('order')->get();
 
         return view('welcome');//, ['posts' => $posts]);//, 'slides' => $slides]);
     }
