5fab8cb65bc98d208d9b7654d1d9398b484fd30b
Raúl Caro Pastorino <dev@fryntiz.es>
2019-10-14 16:25:30 +0200

    Estilos personalizados para content builder

---
+++ resources/views/layouts/contentbuilder_styles_front.blade.php
@@ -0,0 +1,5 @@
+<link href="{{asset('css/front_contentbuilder_custom/minimalist-blocks-content.css')}}" rel="stylesheet"
+      type="text/css"/>
+<link href="{{asset('css/front_contentbuilder_custom/contentbuilder.css')}}"
+      rel="stylesheet"
+      type="text/css"/>
