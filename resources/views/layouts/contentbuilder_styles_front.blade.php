a89ef06abb1c7d439229523e54bb49bb9f6aca9c
Raúl Caro Pastorino <dev@fryntiz.es>
2019-11-13 07:36:19 +0100

    Añado iconos para hoja de estilos para resetear valores dentro del contentbuilder

--- resources/views/layouts/contentbuilder_styles_front.blade.php
+++ resources/views/layouts/contentbuilder_styles_front.blade.php
@@ -1,5 +1,15 @@
-<link href="{{asset('css/front_contentbuilder_custom/minimalist-blocks-content.css')}}" rel="stylesheet"
-      type="text/css"/>
+<link href="{{asset('css/front_contentbuilder_custom/reset-styles.css')}}"
+      rel="stylesheet"
+      type="text/css" />
+
 <link href="{{asset('css/front_contentbuilder_custom/contentbuilder.css')}}"
       rel="stylesheet"
-      type="text/css"/>
+      type="text/css" />
+
+<link href="{{asset('css/front_contentbuilder_custom/minimalist-blocks-content.css')}}"
+      rel="stylesheet"
+      type="text/css" />
+
+<link href="{{asset('css/ionicons/css/ionicons.min.css')}}"
+      rel="stylesheet"
+      type="text/css" />
