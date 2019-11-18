034c24dca391588d0fd01ce4f5757f53adf4b3c0
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-11-19 00:21:31 +0100

    Reorganizo estructura y añado protección al administrador xerintel

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -2,14 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Admin\BaseFiltradoController;
 use App\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\URL;
 use TCG\Voyager\Http\Controllers\VoyagerBaseController;
 
-class UserController extends VoyagerBaseController
+class UserController extends BaseFiltradoController
 {
+    /**
+     * Añado filtro para que el usuario Xerintel quede protegido y vea a todos.
+     * Los administradores puede ver a todos menos a xerintel.
+     */
+    protected function miFiltro(Builder $query)
+    {
+        $role_id = \auth()->user()->role_id;
+
+        if ($role_id == 1) {
+            return $query;
+        } else if ($role_id == 3) {
+            return $query->whereNotIn('role_id', [1]);
+        }
+
+        // Esta línea solo deja ver usuarios que no son administradores.
+        //return $query->whereNotIn('role_id', [1, 3]);
+
+        return $query->whereNotIn('role_id', [1]);
+    }
+
     public function update(Request $request, $id)
     {
         $this->onlyAdminCanSetAdmin($request);
