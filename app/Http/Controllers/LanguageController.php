febfb23018625802b033e1e52b08d5949d6fce11
Raúl Caro Pastorino <dev@fryntiz.es>
2020-04-06 11:28:40 +0200

    Mejoras en traducciones, añado ruta y mejor control en la función que procesa

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App;
 use Illuminate\Http\Request;
 use Session;
 use URL;
+use function in_array;
+use function redirect;
 
 class LanguageController extends Controller
 {
     public function setLocale($locale = 'es')
     {
+        if (!in_array($locale, ['es', 'en'])){
+            $locale = 'es';
+        }
+
         Session::put('locale', $locale);
+        //App::setLocale($locale);
+
         return redirect(url(URL::previous()));
     }
 }
