fb5e910f29f09f3f5bcdefae36ca825ca92aace6
Raúl Caro Pastorino <dev@fryntiz.es>
2020-04-10 15:19:58 +0200

    Añado configuración web para la empresa

---
+++ app/WebSetting.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WebSetting extends SimpleModel
+{
+    protected $table = 'web_settings';
+}
---
+++ database/migrations/2020_01_23_114845_create_web_settings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWebSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('web_settings', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->increments('id');
+            $table->string('field', 511)->comment('Nombre del campo');
+            $table->text('value')->comment('Valor del campo');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('web_settings');
+    }
+}
