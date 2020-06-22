8f1c0fdf91c76cbcfb5ae4edc8a6024d22929578
Raúl Caro Pastorino <dev@fryntiz.es>
2020-06-22 08:26:18 +0200

    Añado ejemplo para extender edit-add.blade.php de voyager

---
+++ resources/views/vendor/voyager/custom_views_extend/examples/edit-add.blade.php
@@ -0,0 +1,39 @@
+@extends('vendor.voyager.custom_views_extend.edit-add')
+
+@section('buttons_custom_top')
+
+@endsection
+
+{{-- Bloque de contenido antes del formulario --}}
+@section('content_custom_top')
+    <div class="row" style="margin-top: 30px">
+        <div class="col-md-12">
+
+        </div>
+    </div>
+@endsection
+
+{{-- Bloque de contenido justo al comenzar formulario <form> --}}
+@section('content_custom_form_top')
+
+@endsection
+
+{{-- Bloque de contenido justo antes de terminar formulario </form> --}}
+@section('content_custom_form_bottom')
+
+@endsection
+
+{{-- Bloque de contenido en el footer del formulario para añadir botones --}}
+@section('content_custom_form_buttons')
+    {{-- Justo al lado del botón "Guardar" que hace el submit --}}
+
+@endsection
+
+{{-- Bloque de contenido una vez acabado formulario --}}
+@section('content_custom_bottom')
+    <div class="row" style="margin-top: 30px">
+        <div class="col-md-12">
+
+        </div>
+    </div>
+@endsection
